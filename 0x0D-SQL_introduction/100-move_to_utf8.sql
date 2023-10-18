7b2d1e7d00b6de525bdffa4eb25380c671075704
GechCodes <xblurayne30@gmail.com>
2023-10-17 21:07:26 -0700

    my sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,12 @@
+-- Convert the hbtn_0c_0 database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Switch to the hbtn_0c_0 database
+USE hbtn_0c_0;
+
+-- Convert the first_table table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the name field in the first_table table to UTF8
+ALTER TABLE first_table CHANGE name name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
