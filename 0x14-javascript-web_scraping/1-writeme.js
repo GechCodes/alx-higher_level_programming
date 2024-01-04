cd643096b16d067543f2e0a374aa0df3622599eb
GechCodes <xblurayne30@gmail.com>
2024-01-03 22:16:07 -0800

    web scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filename = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filename, data, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const todos = JSON.parse(body);
+    const dash = {};
+    for (const todo of todos) {
+      const status = todo.completed;
+      const key = todo.userId;
+      if (status) {
+        if (dash[key]) {
+          dash[key]++;
+        } else {
+          dash[key] = 1;
+        }
+      }
+    }
+    console.log(dash);
+  }
+});
+
